e1bdd53cd3bb40fb0396a233dfc5e53c7451c86b
Ahmet Hakan Altıparmak <ahmethakanaltiparmak@gmail.com>
2024-11-01 17:45:35 +0300

    Responsive dizayn ayarlandı.

--- client/src/components/whoareus/Whoareus.jsx
+++ client/src/components/whoareus/Whoareus.jsx
@@ -7,18 +7,18 @@ const Whoareus = () => {
         <div className="whoareus-container">
             <Navbar />
             <div className="content">
-                <h2 className='whoh2'>Biz Kimiz ?</h2>
-                <p className='whop'>
+                <h2 className="whoh2 animate-slide">Biz Kimiz ?</h2>
+                <p className="whop animate-fade">
                     Biz eğitim odaklı bir platformuz. Kullanıcılarımız, deneme sınavı sonuçlarını bizimle paylaşabilirler.
                     Bu sonuçlar, Gemini yapay zekamız tarafından analiz edilerek kullanıcılarımıza özel geri bildirimler sağlanacaktır.
                 </p>
-                <h2 className='whoh2'>Misyonumuz</h2>
-                <p className='whop'>
+                <h2 className="whoh2 animate-slide">Misyonumuz</h2>
+                <p className="whop animate-fade">
                     Eğitimde erişilebilirliği artırmak ve her bireyin potansiyelini en üst düzeye çıkarmak için 
                     yenilikçi ve etkili çözümler sunmak.
                 </p>
-                <h2 className='whoh2'>Vizyonumuz</h2>
-                <p className='whop'>
+                <h2 className="whoh2 animate-slide">Vizyonumuz</h2>
+                <p className="whop animate-fade">
                     Eğitim teknolojilerinde lider bir platform olmak ve her öğrencinin başarılı bir geleceğe ulaşmasını sağlamak.
                 </p>
             </div>
